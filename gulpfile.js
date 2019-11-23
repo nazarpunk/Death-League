1bec3d49ce92bf94761c213f62cfa86d78d2335b
nazarpunk <nazarpunk@ya.ru>
2019-11-23 15:49:38 +0200

    run

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require(`gulp`);
 const concat = require(`gulp-concat`);
 const exec = require('child_process').exec;
 
+const path = {
+    game: `D:\\Games\\Warcraft III\\x86_64`,
+    map: `test.w3x`
+};
+
 const build = () => {
     return gulp
         .src([
@@ -16,18 +21,30 @@ const watch = () => {
         [
             `build/**/*.lua`
         ],
-        gulp.series(build, run)
+        gulp.series(build, openInGame)
     );
 };
 gulp.task('watch', watch);
 
-const run = (cb) => {
+const open = (cb, isGame) => {
+    const exe = isGame ? `Warcraft III.exe` : `World Editor.exe`;
 
-    exec(`start "" "C:\\Program Files (x86)\\foobar2000\\foobar2000.exe"`, (err, stdout, stderr) => {
-        console.log(stdout);
-        console.warn(stderr);
-        cb(err)
-    });
-    return gulp;
+    exec(`start "" "${path.game}\\${exe}" -loadfile "%cd%\\${path.map}"`, (err, stdout, stderr) => {
+            console.log(stdout);
+            console.warn(stderr);
+            cb(err);
+        }
+    );
 };
-gulp.task('run', run);
+
+gulp.task('build', build);
+
+const openInGame = (cb) => {
+    open(cb, true);
+};
+gulp.task('open in game', openInGame);
+
+const openInEditor = (cb) => {
+    open(cb, false);
+};
+gulp.task('open in editor', openInEditor);
