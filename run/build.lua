5b92f4c5fa45495a658c100ef18b950e4d9717fa
nazarpunk <nazarpunk@ya.ru>
2019-11-28 17:11:42 +0200

    build 3

--- run/build.lua
+++ run/build.lua
@@ -1,17 +1,17 @@
 require 'lfs'
 local param = {
-	game    = [[D:\Games\Warcraft III\x86_64]], -- папка с игрой
-	map     = [[\map.w3x]], -- папка с картой
-	output  = [[\custom-code.lua]], -- файл, в который собирается весь код
-	patcher = [[\custom-code-replacer.exe]], -- патчер для .wct
-	lib     = [[\run\lib]], -- папка с нужными для сборки функциями
-	files   = { -- порядок сборки файлов
+	game       = [[D:\Games\Warcraft III\x86_64]], -- папка с игрой
+	map        = [[\map.w3x]], -- папка с картой
+	customCode = [[\custom-code.lua]], -- файл, в который собирается весь код
+	patcher    = [[\custom-code-replacer.exe]], -- патчер для .wct
+	lib        = [[\run\lib]], -- папка с нужными для сборки функциями
+	files      = { -- порядок сборки файлов
 		[[\build\Ability]],
 		[[\build\Libs]],
 		[[\build\Init.lua]],
 	},
-	tag     = [[--CUSTOM_CODE]], -- тэг для вставки кода
-	current = lfs.currentdir() -- текущая папка проэкта
+	tag        = [[--CUSTOM_CODE]], -- тэг для вставки кода
+	current    = lfs.currentdir() -- текущая папка проэкта
 }
 
 -- подключаем нужные функции
@@ -22,39 +22,39 @@ for file in lfs.dir(param.current .. param.lib) do
 end
 
 -- собираем всё в один файл
-local output = io.open(param.current .. param.output, 'w+')
-output:write(param.tag, '\n')
+local customCode = io.open(param.current .. param.customCode, 'w+')
+customCode:write(param.tag, '\n')
 for i = 1, #param.files do
 	local file = param.files[i]
 	local path = param.current .. file
 	if file:match "[^.]+$" == 'lua' then
-		output:write(FileContent(path), '\n')
+		customCode:write(FileContent(path), '\n')
 	else
 		for filepath, attr in DirTree(path) do
 			if (attr.mode == 'file') then
-				output:write(FileContent(filepath), '\n')
+				customCode:write(FileContent(filepath), '\n')
 			end
 		end
 	end
 end
 
-output:write(param.tag)
-output:close()
+customCode:write(param.tag)
+customCode:close()
 
 -- заменяем код в war3map.lua
 local path    = param.current .. param.map .. '\\war3map.lua'
 local war3map = io.open(path, 'r')
-output        = io.open(param.current .. param.output, 'r')
+customCode    = io.open(param.current .. param.customCode, 'r')
 local content = war3map:read('*a')
 war3map:close()
 war3map           = io.open(path, 'w+')
-local repl, count = string.gsub(content, param.tag .. '.*' .. param.tag, output:read('*a'))
+local repl, count = string.gsub(content, param.tag .. '.*' .. param.tag, customCode:read('*a'))
 war3map:write(repl)
 war3map:close()
-output:close()
+customCode:close()
 
 -- патчим .wct
-os.execute('start "" "' .. param.current .. param.patcher .. '" "' .. param.current .. param.map .. '\\war3map.wct" "' .. param.current .. param.output .. '"')
+os.execute('start "" "' .. param.current .. param.patcher .. '" "' .. param.current .. param.map .. '\\war3map.wct" "' .. param.current .. param.customCode .. '"')
 
 -- запускаем игру
 if type(IsRunGame) == 'boolean' then
